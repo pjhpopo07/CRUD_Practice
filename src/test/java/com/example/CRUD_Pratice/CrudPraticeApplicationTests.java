a704e084211c5c15aa682bff30b65c72cc189142
pjhpopo07 <pjhpopo07@gmail.com>
2025-04-25 22:01:08 +0900

    CRUD_PRACTICE 프로젝트 생성

---
+++ src/test/java/com/example/CRUD_Pratice/CrudPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUD_Pratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
