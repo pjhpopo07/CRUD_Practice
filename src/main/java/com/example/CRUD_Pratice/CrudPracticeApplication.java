e8e2bfae94703bfe22e477ab774f9bd0648a5f40
pjhpopo07 <pjhpopo07@gmail.com>
2025-04-26 15:33:58 +0900

    2장 문제풀이 해결

--- src/main/java/com/example/CRUD_Pratice/CrudPracticeApplication.java
+++ src/main/java/com/example/CRUD_Pratice/CrudPracticeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudPraticeApplication {
+public class CrudPracticeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrudPraticeApplication.class, args);
+		SpringApplication.run(CrudPracticeApplication.class, args);
 	}
 
 }
